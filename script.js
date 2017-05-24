8dfdb75e0a6ff196879d8b916468e0715cccd464
Laura Caroselli <lcaroselli@outlook.com>
2017-05-23 22:14:47 -0600

    Add clear text forms after submitting an idea

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 //Global Variables
-var ideaCopy = $('.body'); //body input
-var ideaHeader = $('.title'); //header input
-var saveBtn = $('.save'); //save button
-var ideaList = $('.card-box'); //section holding our ideas
-var ideaDiv = $('.idea-card'); //article area for card info
+var ideaCopy = $('.body');
+var ideaHeader = $('.title');
+var saveBtn = $('.save');
+var ideaList = $('.card-box');
+var ideaDiv = $('.idea-card');
 
-var ideaArray =[]; //our empty array to hold our idea objects; we should make it so it fetches what's already in our local storage first
+var ideaArray =[];
 
 
 //Functions
-//Document ready state -- saving in local storage, need to append to DOM
+//Document ready state
 $(document).ready(function() {
 	loadIdeas();
 });
 
 function storeIdea() {
-	localStorage.setItem('ideaBoxArray', JSON.stringify(ideaArray)); //Sets the stringified array to the key 'IdeaBoxArray'
+	localStorage.setItem('ideaBoxArray', JSON.stringify(ideaArray));
 }
 
 function loadIdeas() {
 	ideaArray = JSON.parse(localStorage.getItem('ideaBoxArray')) || [];
-		for (var i = 0; i < ideaArray.length; i++) {
+		for (var i = 0; i < ideaArray.length; i++) {  //Refactor into foreach
 			ideaCardOutput(ideaArray[i]);
 		}
 };
@@ -28,47 +28,35 @@ function loadIdeas() {
 
 //Constructor Function
 	function newIdea(title, body) {
-		this.title=title;
-		this.body=body;
-		this.id=Math.floor(Math.random() * 999999);
-		this.quality='swill';
+		this.title = title;
+		this.body = body;
+		this.id = Math.floor(Math.random() * 999999);
+		this.quality = 'swill';
 	}
 
-
 	function ideaCardOutput (newIdeaObject) {
-		var newArticle = document.createElement('article');
-  		newArticle.innerHTML =
-				`<h3> ${newIdeaObject.title} </h3>
-	  		<img class="delete-btn" height="20px" width="20px">
-	  		<p class="body-text"> ${newIdeaObject.body}</p>
-	  		<img class="upvote-btn" height="20px" width="20px">
-	  		<img class="downvote-btn" height="20px" width="20px">
-	  		<p id="quality">quality: <span class="quality-rating"> ${newIdeaObject.quality} </span></p>
-	  		<hr>`;
-
-  		return newArticle;
-
+		$('.idea-card').prepend (
+			`<h3> ${newIdeaObject.title} </h3>
+	  	<img class="delete-btn" height="20px" width="20px">
+	  	<p class="body-text"> ${newIdeaObject.body}</p>
+	  	<img class="upvote-btn" height="20px" width="20px">
+	  	<img class="downvote-btn" height="20px" width="20px">
+	  	<p id="quality">quality: <span class="quality-rating"> ${newIdeaObject.quality} </span></p>
+	  	<hr>`);
 		addClassStyling('p', "body-text");
 }
-
 
 	function addClassStyling (element, className) {
 		$('element').addClass("className");
 	}
 
-
 	function saveIdeaCard() {
 		var ideaCard = new newIdea(ideaHeader.val(), ideaCopy.val());
-
-	  //Save array to local
-		//Persist the idea's quality as well
-
 	  var ideaToPrepend = newIdea(ideaCard);
 		ideaArray.push(ideaCard);
 	  ideaDiv.prepend(ideaCardOutput(ideaCard));
-
 		storeIdea();
-
+		//Persist the idea's quality as well
 		//text fields cleared after saving idea
 	}
 
@@ -82,44 +70,69 @@ function loadIdeas() {
 				return 'genius';
 			}
 		}
-		//Tie the Quality value to unique ID of each card
-		//Swill ++ -> plausible
-		//Plausible ++ -> Genius
-		//Genius ++ -> No effect
-		//Genius -- -> plausible
-		//Plausible -- -> swill
-		//Swill -- -> No effect
-		//Inner text of swill to change upon increment
 
+	function downQuality(quality){
+		switch (quality) {
+			case 'genius':
+				return 'plausible';
+			case 'plausible':
+				return 'swill';
+			case 'swill':
+				return 'TESTING';
+		}
+	}
 
+	// function ...(){
+		//Target a specific object instance on our array
+		//Capture the span element on that specific object instance
+			//<p id="quality">quality: <span class="quality-rating"> ${newIdeaObject.quality} </span></p>
+		//Target the text of that span
+		//Update the text of that span to change according to the downvote/upvote sequence
+		//save the quality changes to the DOM and to the localStorage
+	// }
+
+	//Tyler
 	function deleteIdeaCard() {
 		//need to target the specific Id of the card and delete that card upon clicking the 'x' btn
 		//remove from DOM
 		//remove from local storage
 	}
 
+	//Me
 	function filterIdeaCard() {
 		//filter array method
 		//As user types in search box, the list should filter in real time to ideas whose title or body include user's text; Clearing search box should restore all ideas to field
 	}
 
-	function fetchStorageItems() {
-	// 	local storage to parse, to map
-	// 	push those items into array on page loadd
-	}
-
-
+function resetForm() {
+	$('.title').val('');
+	$('.body').val('');
+	$('.title').focus();
+}
 
 //Events
-$('.save').on('click', saveIdeaCard);
+$('.save').on('click', function () {
+	saveIdeaCard();
+	event.preventDefault();
+	resetForm();
+});
+
 $('body').on('click', '.delete-btn', function() {
 		console.log('delete');
 	});
+
 $('body').on('click', '.downvote-btn', function() {
+		var objectQuality = $();
+		downQuality(objectQuality);
+		console.log(objectQuality);
 		console.log('downvote');
 	});
+
 $('body').on('click', '.upvote-btn', function() {
+		var objectQuality = $(ideaArray[quality]);
+		upQuality (objectQuality);
 		console.log('upvote');
 	});
 
+//Tyler
 //Click event --> click text fields on card, target that text to edit; when user clicks title or body, text should become an editable field pre-populated with existing text; changes saved by clicking outside of text field or pressing enter/return key; edits reflected in storage
