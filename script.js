1f352741868ee2bb2f2175649fcbb497dffea17e
Laura Caroselli <lcaroselli@outlook.com>
2017-05-24 15:29:20 -0600

    Quality text wired up to upvote and downvote buttons.

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ var ideaHeader = $('.title');
 var saveBtn = $('.save');
 var ideaList = $('.card-box');
 var ideaDiv = $('.idea-card');
-
 var ideaArray =[];
 
 
@@ -27,88 +26,86 @@ function loadIdeas() {
 
 
 //Constructor Function
-	function newIdea(title, body) {
-		this.title = title;
-		this.body = body;
-		this.id = Math.floor(Math.random() * 999999);
-		this.quality = 'swill';
-	}
-
-	function ideaCardOutput (newIdeaObject) {
-		$('.idea-card').prepend (
-			`<h3> ${newIdeaObject.title} </h3>
-	  	<img class="delete-btn" height="20px" width="20px">
-	  	<p class="body-text"> ${newIdeaObject.body}</p>
-	  	<img class="upvote-btn" height="20px" width="20px">
-	  	<img class="downvote-btn" height="20px" width="20px">
-	  	<p id="quality">quality: <span class="quality-rating"> ${newIdeaObject.quality} </span></p>
-	  	<hr>`);
-		addClassStyling('p', "body-text");
+function newIdea(title, body) {
+	this.title = title;
+	this.body = body;
+	this.id = Math.floor(Math.random() * 999999);
+	this.quality = 'swill';
 }
 
-	function addClassStyling (element, className) {
-		$('element').addClass("className");
+function ideaCardOutput (newIdeaObject) {
+	$('.idea-card').prepend (
+		`<h3> ${newIdeaObject.title} </h3>
+	  <img class="delete-btn" height="20px" width="20px">
+	  <p class="body-text"> ${newIdeaObject.body}</p>
+	  <img class="upvote-btn" height="20px" width="20px">
+	  <img class="downvote-btn" height="20px" width="20px">
+	  <p id="quality">quality: <span class="quality-rating"> ${newIdeaObject.quality} </span></p>
+	  <hr>`);
+	addClassStyling('p', "body-text");
+}
+
+function addClassStyling (element, className) {
+	$('element').addClass("className");
+}
+
+function saveIdeaCard() {
+	var ideaCard = new newIdea(ideaHeader.val(), ideaCopy.val());
+	var ideaToPrepend = newIdea(ideaCard);
+	ideaArray.push(ideaCard);
+	ideaDiv.prepend(ideaCardOutput(ideaCard));
+	storeIdea();
+	//Persist the idea's quality as well
+}
+
+function upQuality(quality){
+	switch (quality) {
+		case 'swill':
+	  	return 'plausible';
+	  case 'plausible':
+	    return 'genius';
+	  case 'genius':
+			return 'genius';
 	}
+}
 
-	function saveIdeaCard() {
-		var ideaCard = new newIdea(ideaHeader.val(), ideaCopy.val());
-	  var ideaToPrepend = newIdea(ideaCard);
-		ideaArray.push(ideaCard);
-	  ideaDiv.prepend(ideaCardOutput(ideaCard));
-		storeIdea();
-		//Persist the idea's quality as well
-		//text fields cleared after saving idea
+function downQuality(quality){
+	switch (quality) {
+		case 'genius':
+			return 'plausible';
+		case 'plausible':
+			return 'swill';
+		case 'swill':
+			return 'swill';
 	}
+}
 
-	function upQuality(quality){
-		switch (quality) {
-	   	case 'swill':
-	   		return 'plausible';
-	   	case 'plausible':
-	      return 'genius';
-	    case 'genius':
-				return 'genius';
-			}
-		}
+function filterIdeaCard() {
+	// var filteredArray = ideaArray.filter(function(obj) {
+	// As a user types in the search box, the list of ideas should filter in real time to only display ideas whose title or body include the user’s text. The page should not reload.
+	// Clearing the search box should restore all the ideas to the list.
+	// });
+}
+// Loop through all list items, and hide those who don't match the search query
+//     for (i = 0; i < ideaArray.length; i++) {
+//         a = ...[i].getElementsByTagName("...")[0];
+//         if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+//             ...[i].style.display = "";
+//         } else {
+//             ...[i].style.display = "none";
+//         }
+//     }
+// }
 
-	function downQuality(quality){
-		switch (quality) {
-			case 'genius':
-				return 'plausible';
-			case 'plausible':
-				return 'swill';
-			case 'swill':
-				return 'TESTING';
-		}
-	}
 
-	// function ...(){
-		//Target a specific object instance on our array
-		//Capture the span element on that specific object instance
-			//<p id="quality">quality: <span class="quality-rating"> ${newIdeaObject.quality} </span></p>
-		//Target the text of that span
-		//Update the text of that span to change according to the downvote/upvote sequence
-		//save the quality changes to the DOM and to the localStorage
-	// }
-
-	//Tyler
-	function deleteIdeaCard() {
-		//need to target the specific Id of the card and delete that card upon clicking the 'x' btn
-		//remove from DOM
-		//remove from local storage
-	}
-
-	//Me
-	function filterIdeaCard() {
-		//filter array method
-		//As user types in search box, the list should filter in real time to ideas whose title or body include user's text; Clearing search box should restore all ideas to field
-	}
 
 function resetForm() {
 	$('.title').val('');
 	$('.body').val('');
 	$('.title').focus();
 }
+
+
 
 //Events
 $('.save').on('click', function () {
@@ -118,21 +115,31 @@ $('.save').on('click', function () {
 });
 
 $('body').on('click', '.delete-btn', function() {
-		console.log('delete');
-	});
+	console.log('delete');
+});
 
 $('body').on('click', '.downvote-btn', function() {
-		var objectQuality = $();
-		downQuality(objectQuality);
-		console.log(objectQuality);
-		console.log('downvote');
+	ideaArray.forEach(function(object, index, array) {
+		var qualityText = $('.quality-rating')
+		console.log(object.quality);
+		object.quality = downQuality(object.quality);
+		qualityText.innerText = downQuality(object.quality);
 	});
+});
+
 
 $('body').on('click', '.upvote-btn', function() {
-		var objectQuality = $(ideaArray[quality]);
-		upQuality (objectQuality);
-		console.log('upvote');
+	ideaArray.forEach(function(object, index, array) {
+		var qualityText = $('.quality-rating')
+		console.log(object.quality);
+		object.quality = upQuality(object.quality);
+		qualityText.innerText = upQuality(object.quality);
 	});
+});
 
-//Tyler
-//Click event --> click text fields on card, target that text to edit; when user clicks title or body, text should become an editable field pre-populated with existing text; changes saved by clicking outside of text field or pressing enter/return key; edits reflected in storage
+
+//foreach object in our array, grab the id a
+//Target the value (text) of that property
+//Change the value (text) of that property according to the downvote/upvote sequence
+//save the quality changes to the DOM and to the localStorage
+//
