73e51214398ddb85a5d407c3bea08c84c5791e69
Laura Caroselli <lcaroselli@outlook.com>
2017-05-24 17:54:09 -0600

    Push working JSON

--- script.js
+++ script.js
@@ -1,43 +1,55 @@
 //Global Variables
-var ideaCopy = $('.body'); //body input
-var ideaHeader = $('.title'); //header input
-var saveBtn = $('.save'); //save button
-var ideaList = $('.card-box'); //section holding our ideas
-var ideaDiv = $('.idea-card'); //article area for card info
-
-var ideaArray =[]; //our empty array to hold our idea objects
-
+var ideaCopy = $('.body');
+var ideaHeader = $('.title');
+var saveBtn = $('.save');
+var ideaList = $('.card-box');
+var ideaDiv = $('.idea-card');
+var ideaArray =[];
 
 
 //Functions
-//Constructor Function
-	function newIdea(title, body) {
-		this.title=title;
-		this.body=body;
-		this.id=Math.floor(Math.random() * 999999);
-	}
+//Document ready state
+$(document).ready(function() {
+	loadIdeas();
+});
 
-
-	function ideaCardOutput (newIdeaObject) {
-		var newArticle = document.createElement('article');
-  		newArticle.innerHTML =
-				`<h3> ${newIdeaObject.title} </h3>
-	  		<img class="delete-btn" height="20px" width="20px">
-	  		<p class="body-text"> ${newIdeaObject.body}</p>
-	  		<img class="upvote-btn" height="20px" width="20px">
-	  		<img class="downvote-btn" height="20px" width="20px">
-	  		<p id="quality">quality: <span class="quality-rating"> ${newIdeaObject.quality} </span></p>
-	  		<hr>`;
-
-  		return newArticle;
-
-		addClassStyling('p', "body-text");
+function storeIdea() {
+	localStorage.setItem('ideaBoxArray', JSON.stringify(ideaArray));
 }
 
+function loadIdeas() {
+	ideaArray = JSON.parse(localStorage.getItem('ideaBoxArray')) || [];
+		for (var i = 0; i < ideaArray.length; i++) {  //Refactor into foreach
+			ideaCardOutput(ideaArray[i]);
+		}
+};
 
-	function addClassStyling (element, className) {
-		$('element').addClass("className");
-	}
+
+//Constructor Function
+function newIdea(title, body) {
+	this.title = title;
+	this.body = body;
+	this.id = Math.floor(Math.random() * 999999);
+	this.quality = 'swill';
+}
+
+function ideaCardOutput (newIdeaObject) {
+	$('.idea-card').prepend (
+		`<article>
+			<h3> ${newIdeaObject.title} </h3>
+		  <img class="delete-btn" height="20px" width="20px">
+		  <p class="body-text"> ${newIdeaObject.body}</p>
+		  <img class="upvote-btn" height="20px" width="20px">
+		  <img class="downvote-btn" height="20px" width="20px">
+		  <p id="quality">quality: <span class="quality-rating"> ${newIdeaObject.quality} </span></p>
+		  <hr>
+		</article>`);
+	addClassStyling('p', "body-text");
+}
+
+function addClassStyling (element, className) {
+	$('element').addClass("className");
+}
 
 
 	function saveIdeaCard() {
